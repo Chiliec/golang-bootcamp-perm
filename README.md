f03056330793b67cd25b95dd52f30d336ed3eacd
Бабин Владимир <vovababin@gmail.com>
2017-08-26 10:16:39 +0500

    Add report card badge

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # golang-bootcamp-perm
 
+[![Go Report Card](https://goreportcard.com/badge/github.com/Chiliec/golang-bootcamp-perm)](https://goreportcard.com/report/github.com/Chiliec/golang-bootcamp-perm)
+
 Just a test task for [Golang Bootcamp Perm](https://www.meetup.com/GDG-Perm/events/242030053/).
--- models/Valute.go
+++ models/Valute.go
@@ -1,9 +1,9 @@
 package models
 
 type Valute struct {
-	NumCode int `xml:"NumCode"`
+	NumCode  int    `xml:"NumCode"`
 	CharCode string `xml:"CharCode"`
-	Nominal int `xml:"Nominal"`
-	Name string `xml:"Name"`
-	Value string `xml:"Value"`
+	Nominal  int    `xml:"Nominal"`
+	Name     string `xml:"Name"`
+	Value    string `xml:"Value"`
 }
